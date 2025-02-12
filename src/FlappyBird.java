2e3989c138f5eb473be808c7170f525f696b3204
Ali Al-Jalo <ali.aljalo@gmail.com>
2025-02-12 16:10:45 +0300

    Useing the images

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -10,9 +10,21 @@ public class FlappyBird extends JPanel{
     int boardWidth = 360;
     int boardHeight = 640;
 
+    //Images
+    Image backgroundImage;
+    Image birdImage;
+    Image topPipeImage;
+    Image bottomPipeImage;
+
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.blue);
+        // setBackground(Color.blue);
+
+        //load images
+        backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImage = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
     }
 
 }
