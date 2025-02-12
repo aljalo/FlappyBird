1a1c1f2c82299cef5b43d7739d131320cca4d056
Ali Al-Jalo <ali.aljalo@gmail.com>
2025-02-12 18:31:19 +0300

    Create GameOver

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -67,6 +67,7 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
 
     Timer gameLoop;
     Timer placePipesTimer;
+    boolean gameOver = false;
 
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -144,12 +145,31 @@ public void draw(Graphics g){
         for (int i = 0; i < pipes.size(); i++) {
             Pipe pipe = pipes.get(i);
             pipe.x += velocityX;
+
+            if(collision(bird, pipe)){
+                gameOver = true;
+            }
+        }
+        if(bird.y > boardHeight){
+            gameOver = true;
         }
     }
+
+    public boolean collision(Bird a, Pipe b){
+        return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
+                a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
+                a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
+                a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         move();
         repaint();
+        if(gameOver){
+            placePipesTimer.stop();
+            gameLoop.stop();
+        }
     }
 
 
