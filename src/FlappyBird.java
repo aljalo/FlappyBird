41c2722b59b2ea28b03be2070303bb1948435a64
Ali Al-Jalo <ali.aljalo@gmail.com>
2025-02-12 17:36:26 +0300

    Create move

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird extends JPanel implements ActionListener{
+public class FlappyBird extends JPanel implements ActionListener, KeyListener{
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -20,10 +20,8 @@ public class FlappyBird extends JPanel implements ActionListener{
     int birdWidth = 34;
     int birdHeight =24;
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        repaint();
-    }
+
+
 
     class Bird{
         int x = birdX;
@@ -38,8 +36,25 @@ public class FlappyBird extends JPanel implements ActionListener{
 
     }
 
+    //Pipes
+    int pipeX = boardWidth;
+    int pipeY = 0;
+    int pipeWidth = 64;
+    int pipeHeight = 512;
+
+    class Pipe{
+        int x = pipeX;
+        int y = pipeY;
+        int width = pipeWidth;
+        int height = pipeHeight;
+        Image img;
+    }
+
+
     //Game Logic
     Bird bird;
+    int velocityY = 0;
+    int gravity = 1;
 
     Timer gameLoop;
 
@@ -47,6 +62,9 @@ public class FlappyBird extends JPanel implements ActionListener{
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         // setBackground(Color.blue);
+        setFocusable(true);
+        addKeyListener(this);
+
 
         //load images
         backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
@@ -67,6 +85,8 @@ public void paintComponent(Graphics g){
         draw(g);
 }
 public void draw(Graphics g){
+        //System.out.println("draw");
+
         //Background
     g.drawImage(backgroundImage, 0,0, boardWidth, boardHeight, null);
 
@@ -74,4 +94,32 @@ public void draw(Graphics g){
     //Bird
     g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
 }
+    public void move(){
+        //Bird
+        velocityY += gravity;
+        bird.y += velocityY;
+        bird.y = Math.max(bird.y, 0);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE){
+            velocityY = -9;
+        }
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
